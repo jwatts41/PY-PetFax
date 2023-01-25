c4ea63328e58238e3825f2e6d8509e8d64f07a21
jwatts41 <wattsjonovan@yahoo.com>
2023-01-25 17:33:20 -0500

    blueprints and views

--- app.py
+++ app.py
@@ -1,2 +1,2 @@
 from petfax import create_app
-app = create_app
+app = create_app()
---
+++ petfax/pet.py
@@ -0,0 +1,11 @@
+import json
+from flask import ( Blueprint, render_template ) 
+
+pets = json.load(open('pets.json'))
+print(pets)
+
+bp = Blueprint('pet', __name__, url_prefix="/pets")
+
+@bp.route('/')
+def index(): 
+    return render_template('index.html', pets=pets)
