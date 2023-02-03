8a9d97a2f4c7c7c8e946518ec8cf882a91ad5ee4
jwatts41 <wattsjonovan@yahoo.com>
2023-02-03 18:19:12 -0500

    updated facts and pets folder, and added in a fact file

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask 
 
-def create_app():
+def create_app(): 
     app = Flask(__name__)
 
     @app.route('/')
-    def hello():
+    def index(): 
         return 'Hello, PetFax!'
-
+   
     from . import pet
     app.register_blueprint(pet.bp)
-    
-    return app    
+
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+    return app
